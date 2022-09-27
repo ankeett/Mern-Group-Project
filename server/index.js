ceacea372c2643ff49eb39f5305b24a99bd3e262
Ankit Kafle <ankeett@Ankits-MacBook-Pro.local>
2022-09-27 00:21:56 -0400

    backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
 
@@ -8,6 +10,13 @@ app.get('/',(req,res)=>{
     res.send('App is running.');
 });
 
-const PORT = 5000;
+const PORT = 5050;
+const CONNECTION_URL = process.env.CONNECTION_URL
 
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
+mongoose.connect(CONNECTION_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>
+    app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+)
+.catch((err)=>console.log(err))
