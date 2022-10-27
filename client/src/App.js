2e6a6d835d528d1c40a3097601b8aa635299219b
Sanskar-Adhikari <adhikarisanskar99@gmail.com>
2022-10-26 18:29:03 -0700

    test

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from me -- test from sanskar
+          Learn React from me -- test from sanskar/ankeet . 
         </a>
       </header>
     </div>
