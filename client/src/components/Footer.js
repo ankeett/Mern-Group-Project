5c9644c0d7c0765ea05c5293b89d7c483fbd6840
Sanskar-Adhikari <adhikarisanskar99@gmail.com>
2022-10-27 17:11:36 -0700

    message

---
+++ client/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box, Stack, Typography} from '@mui/material';
+import Logo from '../assets/images/Logo-1.png'
+const Footer = () => {
+  return (
+    <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack gap="40px"  alignItems= 'center' px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '40px' }} />
+    </Stack>
+    <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" 
+    pb="40px">Made with ❤️ by Sanskar</Typography>
+  </Box>
+);
+}
+
+export default Footer
